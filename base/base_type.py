c4eddab8f6e159bc9b2802b4c82a5272bcb0e546
esennesh <elisennesh@gmail.com>
2020-03-31 20:47:36 -0400

    base.base_type: add base_type file with FirstOrderType
    Signed-off-by: esennesh <elisennesh@gmail.com>

---
+++ base/base_type.py
@@ -0,0 +1,28 @@
+from adt import adt, Case
+import torch
+import uuid
+
+@adt
+class FirstOrderType:
+    TENSORT: Case[torch.dtype, torch.Size]
+    VART: Case[str]
+    ARROWT: Case["FirstOrderType", "FirstOrderType"]
+
+    def _pretty(self, parenthesize=False):
+        result = self.match(
+            TENSORT=lambda dtype, size: '%s[%s]' % (dtype, size),
+            vart=lambda name: name,
+            arrowt=lambda l, r: '%s -> %s' % (l._pretty(True), r._pretty())
+        )
+        if parenthesize and self._key == FirstOrderType._Key.ARROWT:
+            result = '(%s)' % result
+        return result
+
+    def __str__(self):
+        return self._pretty(False)
+
+    def __repr__(self):
+        return str(self)
+
+    def __hash__(self):
+        return hash(str(self))
