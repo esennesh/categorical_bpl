model.model.VAECategoryModel: correct normalization of morphism weights
Now that we return the long-run unnormalized probabilities of arriving to an
object from another object, instead of the negative-log of that (the intuitive
distance itself), switch from a softmin (on intuitive distance to destination)
to a softmax (on long-run arrival probability to destination).

Signed-off-by: esennesh <elisennesh@gmail.com>

model/model.py
@@ -277,8 +277,7 @@ class VAECategoryModel(BaseModel):
         j = torch.LongTensor([self._object_index(neighbor) for (neighbor, _) in
                               morphisms]).to(device=device)
         to_dest = distances.index_select(0, j)[:, dest_idx] * confidence
-
-        morphism_cat = dist.Categorical(probs=F.softmin(to_dest, dim=0))
+        morphism_cat = dist.Categorical(probs=F.softmax(to_dest, dim=0))
         k = pyro.sample('arrow_%d' % k, morphism_cat, infer=infer)
 
         return morphisms[k.item()]