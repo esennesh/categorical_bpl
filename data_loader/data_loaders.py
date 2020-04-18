d354ec6f891442eda9e74d99c2f890f6536348f4
esennesh <elisennesh@gmail.com>
2020-04-17 22:45:37 -0400

    data_loaders.FashionMnistDataLoader: add data-loader for Fashion MNIST
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -53,3 +53,14 @@ class OmniglotTargetBatchDataLoader(BaseTargetBatchDataLoader):
         super().__init__(self.dataset, batch_size, shuffle, validation_split,
                          num_workers, self.targets, drop_train_last=False,
                          drop_valid_last=False)
+
+class FashionMnistDataLoader(BaseDataLoader):
+    """
+    Fashion-MNIST data loading demo using BaseDataLoader
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0,
+                 num_workers=1, training=True):
+        self.dataset = datasets.FashionMNIST(self.data_dir, train=training,
+                                             download=True, transform=trsfm)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split,
+                         num_workers)
