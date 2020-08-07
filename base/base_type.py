f4bc071e0e6fb2391f4505f42c2d3bc57f343870
esennesh <elisennesh@gmail.com>
2020-08-07 14:05:49 -0400

    base_type: fine-tune printing of tensor types
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- base/base_type.py
+++ base/base_type.py
@@ -6,13 +6,13 @@ import uuid
 
 def _label_dtype(dtype):
     if dtype == torch.int:
-        return 'Z'
+        return '\\mathbb{Z}'
     if dtype == torch.uint8:
         return 'Char'
     if dtype == torch.float:
-        return 'R'
+        return '\\mathbb{R}'
     raise NotImplementedError()
 
 def tensor_type(dtype, size):
-    return closed.CartesianClosed.BASE(Ty('%s^{%s}' % (_label_dtype(dtype),
-                                                       str(size))))
+    return closed.CartesianClosed.BASE(Ty('%s^{%d}' % (_label_dtype(dtype),
+                                                       size)))
