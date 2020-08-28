6e3f5288867e719ac359796936698034cc989598
esennesh <elisennesh@gmail.com>
2020-08-27 22:38:12 -0400

    utils.name_stack: reverse the polarity of name-stacking for latent variables
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- utils/name_stack.py
+++ utils/name_stack.py
@@ -10,7 +10,7 @@ import collections
 from pyro.poutine.messenger import Messenger
 
 def _count_stacked(stack, name, offset=0):
-    count = max(stack[name] - offset, 0)
+    count = max(stack[name] + offset, 0)
     if count:
         return name + "__%d" % count
     return name
@@ -33,7 +33,8 @@ class NamePushMessenger(Messenger):
             name_stack = collections.defaultdict(int)
 
     def _pyro_sample(self, msg):
-        msg["name"] = _count_stacked(self._names, msg["name"])
+        offset = int(not msg["is_observed"])
+        msg["name"] = _count_stacked(self._names, msg["name"], offset=offset)
 
     def _pyro_post_sample(self, msg):
         base_name = _stacked_name_base(msg["name"])
@@ -51,7 +52,7 @@ class NamePopMessenger(Messenger):
             name_stack = collections.defaultdict(int)
 
     def _pyro_sample(self, msg):
-        msg["name"] = _count_stacked(self._names, msg["name"], offset=1)
+        msg["name"] = _count_stacked(self._names, msg["name"])
 
     def _pyro_post_sample(self, msg):
         base_name = _stacked_name_base(msg["name"])
