76bd491458251e59a24288b2b1dc7d4c91389503
esennesh <elisennesh@gmail.com>
2020-09-02 22:22:35 -0400

    model.VaeCategoryModel: severely reduce latent dimensionalities
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- model/model.py
+++ model/model.py
@@ -147,7 +147,7 @@ class VaeCategoryModel(CategoryModel):
 
         # Build up a bunch of torch.Sizes for the powers of two between
         # hidden_dim and data_dim.
-        dims = list(util.powers_of(2, hidden_dim, data_dim))
+        dims = list(util.powers_of(2, hidden_dim, data_dim // 4)) + [data_dim]
         dims.sort()
 
         generators = []
