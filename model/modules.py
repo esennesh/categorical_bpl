8af94c04d254a0dd7f22a77bb2698ba37eda87de
esennesh <elisennesh@gmail.com>
2020-09-24 20:53:22 -0400

    Revert "model.modules.ContinuousBernoulliModel: mean reconstruction from likelihood"
    This reverts commit 8b454e5d1fde0a449d63734b7d9997fee54ea4d1.

--- model/modules.py
+++ model/modules.py
@@ -116,7 +116,7 @@ class ContinuousBernoulliModel(TypedModel):
         bernoulli = dist.ContinuousBernoulli(probs=xs).to_event(1)
         sample = pyro.sample('$%s$' % self._random_var_name, bernoulli)
         if self._likelihood:
-            return bernoulli.mean
+            return xs
         return sample
 
 class RelaxedBernoulliModel(TypedModel):
