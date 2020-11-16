3af75f74528990bcca9d8026971fad94370ef8e1
esennesh <elisennesh@gmail.com>
2020-11-16 17:56:25 -0500

    base.base_type: remove unused imports, correct math brackets around type name
    Signed-off-by: esennesh <elisennesh@gmail.com>

--- base/base_type.py
+++ base/base_type.py
@@ -1,6 +1,5 @@
 from adt import adt, Case
 from discopy.biclosed import Ty
-from discopyro import closed
 import re
 import torch
 import uuid
@@ -20,4 +19,4 @@ def type_size(label):
     return int(match[0])
 
 def tensor_type(dtype, size):
-    return Ty('%s^{%d}' % (_label_dtype(dtype), size))
+    return Ty('$%s^{%d}$' % (_label_dtype(dtype), size))
