c5efcf7a6394b13f0304f2b5c73bc5d5e9fbc887
Eli Sennesh <sennesh.e@northeastern.edu>
2021-07-02 15:58:36 -0400

    data_loaders.ZincMolecularDataLoader: loader for Zinc15 SMILES dataset
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,8 @@
+import deepchem as dc
+from deepchem.feat.molecule_featurizers import OneHotFeaturizer
 import numpy as np
 import os.path as path
+import torch.utils.data
 from torchvision import datasets, transforms
 from base import BaseDataLoader, BaseTargetBatchDataLoader
 
@@ -92,3 +95,20 @@ class FashionMnistTargetBatchDataLoader(BaseTargetBatchDataLoader):
                                              download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split,
                          num_workers, self.dataset.targets)
+
+class ZincMolecularDataLoader(BaseDataLoader):
+    """
+    ZINC data loading using BaseDataLoader
+    """
+    def __init__(self, batch_size, max_length=120, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        self._one_hot = OneHotFeaturizer(max_length=max_length)
+        self._tasks, data, _ = dc.molnet.load_zinc15(
+            featurizer=self._one_hot
+        )
+        self._train, self._valid, self._test = data
+        dataset = self._train if training else self._test
+        dataset = torch.utils.data.TensorDataset(
+            torch.as_tensor(dataset.X, dtype=torch.float),
+            torch.as_tensor(dataset.y, dtype=torch.float)
+        )
+        super().__init__(dataset, batch_size, shuffle, validation_split, num_workers)
