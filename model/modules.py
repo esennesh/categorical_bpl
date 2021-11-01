9c91eb88f4cd9a858575c8b700bce275bf0de861
Eli Sennesh <sennesh.e@northeastern.edu>
2021-11-01 17:05:14 -0400

    model.modules.RecurrentMolecularEncoder: effect signature
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- model/modules.py
+++ model/modules.py
@@ -812,6 +812,10 @@ class RecurrentMolecularEncoder(TypedModel):
         return smiles_type >> embedding_type
 
     @property
+    def effect(self):
+        return self.embedding_dist.effect
+
+    @property
     def name(self):
         smiles_name = 'X^{(%d, %d)}' % (self._max_len, self._charset_len)
         embedding_name = 'Z^{%d}' % self._hidden_dim
