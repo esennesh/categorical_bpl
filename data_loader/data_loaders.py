03e55c561735a28af5c851faeb5bb7d2294c5413
Eli Sennesh <sennesh.e@northeastern.edu>
2023-01-16 16:18:09 -0500

    data_loaders.MnistDataLoader: parameter to use indexed or unindexed MNIST
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -14,10 +14,11 @@ class MnistDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True, indexed=False):
         trsfm = transforms.ToTensor()
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        dataset = IndexedMnist if indexed else datasets.MNIST
+        self.dataset = dataset(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
 
 class MnistTargetBatchDataLoader(BaseTargetBatchDataLoader):
