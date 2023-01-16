ac910b3fc45dd82bb360523133cf283fd7cfb7c2
Eli Sennesh <sennesh.e@northeastern.edu>
2023-01-16 16:19:46 -0500

    data_loaders.MnistTargetBatchDataLoader: switch for indexed or unindexed MNIST
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -25,10 +25,11 @@ class MnistTargetBatchDataLoader(BaseTargetBatchDataLoader):
     """
     MNIST data loading batched by label using BaseTargetBatchDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True, indexed=False):
         trsfm = transforms.ToTensor()
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        dataset = IndexedMnist if indexed else datasets.MNIST
+        self.dataset = dataset(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers, self.dataset.targets)
 
 flip = lambda x : 1 - x
