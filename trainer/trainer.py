6982e9ba65b8fedc2defeaba6827d8876045cc43
Eli Sennesh <sennesh.e@northeastern.edu>
2023-01-30 11:59:38 -0500

    trainer.Trainer: move learning-rate scheduling check under validation pass
    Signed-off-by: Eli Sennesh <sennesh.e@northeastern.edu>

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -135,9 +135,9 @@ class Trainer(BaseTrainer):
         if self.do_validation:
             val_log = self._valid_epoch(epoch)
             log.update(**{'val_'+k : v for k, v in val_log.items()})
+            if self.lr_scheduler is not None:
+                self.lr_scheduler.step(val_log['loss'])
 
-        if self.lr_scheduler is not None:
-            self.lr_scheduler.step(val_log['loss'])
         return log
 
     def _valid_epoch(self, epoch):
